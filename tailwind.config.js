a5b776efb5512fb0e108036241f2a6bb83632962
Cinnamon <cinam0n@proton.me>
2024-01-17 10:38:40 +0000

    first-working-version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,22 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    
+
     extend: {
       colors: {
-      'darkgreen': '#2D3D25',
-      'white': '#FFFAE3'
+        'darkgreen': '#2D3D25',
+        'white': '#FFFAE3'
+      },
+      backgroundImage: {
+        'baner-horizontal': "url('img/background.jpg')",
+      },
+      height: {
+        'box': '10vh'
+      },
+      fontFamily: {
+        'body': 'Signika, sans-serif'
+      }
     },
-    backgroundImage: {
-      'baner-horizontal': "url('img/baner-horizontal.png')",
-    }
-  },
   },
   plugins: [],
 }
